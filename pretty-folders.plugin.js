02a3b9235c6f041c76e9bbc1a4d0a729a6a48891
DogancanYr <90002969+DogancanYr@users.noreply.github.com>
2024-03-19 23:55:35 +0300

    Update pretty-folders.plugin.js
    💀

--- pretty-folders.plugin.js
+++ pretty-folders.plugin.js
@@ -4,7 +4,7 @@
  * @description Applies a folder's icon colour to its background when expanded.
  * @version 1.2.1
  * @source https://github.com/webcrawls/discord
- * @updateUrl https://github.com/webcrawls/discord/edit/master/pretty-folders.plugin.js
+ * @updateUrl https://raw.githubusercontent.com/webcrawls/discord/master/pretty-folders.plugin.js
  */
 module.exports = class MyPlugin {
     start = () => Array.from(document.getElementsByClassName(FOLDER_WRAPPER)).forEach(attachFolder)
